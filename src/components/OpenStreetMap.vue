f55e075136d483ed644d7993facaa09beb759962
Wouter Hisschemöller <email@hisschemoller.com>
2021-01-21 19:11:52 +0100

    Marker clusters added.

--- src/components/OpenStreetMap.vue
+++ src/components/OpenStreetMap.vue
@@ -9,13 +9,20 @@ import { Feature, Map, View } from 'ol';
 import { Point } from 'ol/geom';
 import { Tile, Vector as VectorLayer } from 'ol/layer';
 import { fromLonLat } from 'ol/proj';
-import { OSM, Vector as VectorSource } from 'ol/source';
-import { Icon, Style } from 'ol/style';
+import { Cluster, OSM, Vector as VectorSource } from 'ol/source';
+import {
+  Circle as CircleStyle, Fill, Icon, Stroke, Style, Text,
+} from 'ol/style';
 import 'ol/ol.css';
 import Drawing from '../interfaces/Drawing';
 
+interface StyleCache {
+  [name: string]: Style;
+}
+
 let map: Map;
-let markerLayer: VectorLayer;
+let clusterLayer: VectorLayer;
+const styleCache: StyleCache = {};
 const markerStyle = new Style({
   image: new Icon({
     color: 'rgba(255, 0, 0, .5)',
@@ -29,20 +36,24 @@ const markerStyle = new Style({
  * Create the OpenLayers map.
  */
 function createMap(): void {
+  const tileLayer = new Tile({
+    source: new OSM(),
+  });
+
   map = new Map({
-    layers: [
-      new Tile({
-        source: new OSM(),
-      }),
-    ],
+    layers: [tileLayer],
+    target: 'map',
     view: new View({
       center: [0, 0],
       constrainResolution: true,
       zoom: 0,
     }),
-    target: 'map',
   });
 }
+
+// function getStyle(feature: Feature): StyleFunction {
+//   return new Style({});
+// }
 
 /**
  * Update the markers on the map.
@@ -51,8 +62,8 @@ function updateMarkers(newDrawings: Drawing[]): void {
   const features: Feature[] = [];
 
   // remove the old markers
-  if (markerLayer) {
-    map.removeLayer(markerLayer);
+  if (clusterLayer) {
+    map.removeLayer(clusterLayer);
   }
 
   // add the new markers
@@ -65,12 +76,46 @@ function updateMarkers(newDrawings: Drawing[]): void {
     features.push(feature);
   });
 
-  markerLayer = new VectorLayer({
-    source: new VectorSource({
-      features,
-    }),
+  const source = new VectorSource({
+    features,
   });
-  map.addLayer(markerLayer);
+
+  const clusterSource = new Cluster({
+    distance: 50,
+    source,
+  });
+
+  clusterLayer = new VectorLayer({
+    source: clusterSource,
+    // eslint-disable-next-line object-shorthand, func-names
+    style: function (feature): Style {
+      const size = feature.get('features').length;
+      let style: Style = styleCache[size];
+      if (!style) {
+        style = new Style({
+          image: new CircleStyle({
+            radius: 10,
+            stroke: new Stroke({
+              color: '#fff',
+            }),
+            fill: new Fill({
+              color: size === 1 ? '#006600' : '#3399CC',
+            }),
+          }),
+          text: new Text({
+            text: size.toString(),
+            fill: new Fill({
+              color: '#fff',
+            }),
+          }),
+        });
+        styleCache[size] = style;
+      }
+      return style;
+    },
+  });
+
+  map.addLayer(clusterLayer);
 }
 
 const OpenStreetMap = defineComponent({
